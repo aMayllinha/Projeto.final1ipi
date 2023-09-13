37b554606adac4f8ad65c6f253747a03ce743732
aMayllinha <mayllalima6@gmail.com>
2023-09-13 17:01:26 -0300

    menu inicial

---
+++ app.js
@@ -0,0 +1,27 @@
+var lista_de_produtos = []
+
+while (true) {
+
+    var opcao = prompt(`
+Bem Vindo ao Cadastro de Produtos! 😉🤍
+
+1. Cadastrar o produto
+2. Editar produto
+3. Remover produto
+4. Visualizar os produto
+`)
+
+    if (opcao == 1) {
+        //codigo para adicionar produto
+    } else if (opcao == 2) {
+        //codigo para editar o produto
+    } else if (opcao == 3) {
+        //codigo para remover
+    } else if (opcao == 4) {
+        //codigo para visualizar
+    } else {
+        alert("Nenhuma das opção foram selecionadas")
+    }
+
+
+}
